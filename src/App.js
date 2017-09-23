c2bf5981722a86b5eb0749d16b61c7bff0bf686f
Jonathan Xu <jonathanj.xu97@gmail.com>
2017-09-23 15:19:49 -0400

    Basic editing page (#2)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Route } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import Landing from './scenes/Landing';
 import Navbar from './components/Navbar';
+import Landing from './scenes/Landing';
+import Edit from './scenes/Edit';
 import CardContainer from './components/CardContainer';
 
 const App = () => (
   <div className="container">
     <Route exact path="/" component={CardContainer} />
+    <Route path="/write" component={Edit} />
   </div>
 );
 
